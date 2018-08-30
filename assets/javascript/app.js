ca26c1e2dbe56c06e65c75d89790d5a56eb2941a
kstuart <katieannestuart@gmail.com>
2018-08-30 00:36:24 -0600

    adding css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ function renderButtons() {
     for(var i = 0; i < shows.length; i++) {
         var a = $("<button>");
 
-        a.addClass("show");
+        a.addClass("show btn btn-secondary");
 
         a.attr("data-name", shows[i]);
 
@@ -19,23 +19,24 @@ function renderButtons() {
 }
 
 //add button for a show submitted in the form
-    $("#add-show").on("click", function() {
-        event.preventDefault();
+$("#add-show").on("click", function() {
+    event.preventDefault();
 
-        var show = $("#show-input").val().trim();
+    var show = $("#show-input").val().trim();
 
-        shows.push(show);
-        console.log(shows);
+    shows.push(show);
+    console.log(shows);
 
-        renderButtons();
-        $("#show-input").val("");
-    })
+    renderButtons();
+    $("#show-input").val("");
+})
 
 //display show gifs
 function displayShowGifs() {
     var show = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+    //ajax call to API
     $.ajax ({
         url: queryURL,
         method: "GET"
@@ -44,37 +45,41 @@ function displayShowGifs() {
 
         var results = response.data;
 
-        // var stillImage = ""
-        // var animatedImage = ""
-
+        //loop through results from API 
         for(var j = 0; j < results.length; j++) {
+            
+            //if the rating on the result isn't R or PG-13 create the elements to display the gif on the page
             if(results[j].rating !== "r" && results[j].rating !== "pg-13") {
+
+                //create div to hold the gifs
                 var gifDiv = $("<div class='item'>");
 
+                //variable to hold the gif rating
                 var rating = results[j].rating
                 
+                //create p element to display rating on page
                 var p = $("<p>").text("Rating: " + rating);
 
+                //create an image tag to hold image
                 var showImage = $("<img>");
 
-                // stillImage = results[j].images.fixed_height_still.url
-                // animatedImage = results[j].images.fixed_height.url
-
+                //add a class and data attributes to the img tag. includes data elements make pausing and unpausing the gif possible
                 showImage.addClass("gif");
                 showImage.attr("src", results[j].images.fixed_height_still.url)
                 showImage.attr("data-still", results[j].images.fixed_height_still.url)
                 showImage.attr("data-animate", results[j].images.fixed_height.url)
                 showImage.attr("data-state", "still");
 
-
-                gifDiv.append(p);
+                //append the p and img tags to the gif div
                 gifDiv.append(showImage);
+                gifDiv.append(p);
 
+                //prepend the gif div to the display div in the html
                 $("#gifDisplay").prepend(gifDiv);
-            }
-
-           
+            }  
         }
+
+        //click event that pauses and unpauses the gif when you click on the image
         $(".gif").on("click", function() {
             var state = $(this).attr("data-state");
         
@@ -85,15 +90,15 @@ function displayShowGifs() {
                 $(this).attr("src", $(this).attr("data-still"));
                 $(this).attr("data-state", "still");
             }
-
         })
+
     })
 }
 
-
-
+//click event for the buttons
 $(document).on("click", ".show", displayShowGifs);
 
+//run function to render buttons on initial page load
 renderButtons();
 
 
